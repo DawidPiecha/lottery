627893a042309b43df2335fd1c235e42e785183b
Dawid <dawidpiecha88@gmail.com>
2023-10-16 21:30:50 +0200

    created showResults function. still some functionalities to fix. lottery-page in progress

--- js/lottery-engine.js
+++ js/lottery-engine.js
@@ -1,3 +1,5 @@
+const lotteryResults = document.querySelector(".lottery-results");
+
 const lotteryFunction = () => {
   let results = [];
 
@@ -9,5 +11,23 @@ const lotteryFunction = () => {
     }
   }
 
-  return results;
+  // wywołanie funkcji do wyświetlania wyników i przekazania wyników
+  resultsShow(results);
 };
+
+const resultsShow = (results) => {
+  lotteryResults.innerHTML = "";
+
+  results.forEach((result) => {
+    const drawnNumberDiv = document.createElement("div");
+    const img = document.createElement("img");
+    img.src = `./images/number${result}.png`;
+    img.alt = `Numer ${result}`;
+    drawnNumberDiv.appendChild(img);
+    lotteryResults.appendChild(drawnNumberDiv);
+    startButton.textContent = "LOTTERY  RESULTS:";
+    startButton.disabled = true;
+  });
+};
+
+startButton.addEventListener("click", lotteryFunction);
